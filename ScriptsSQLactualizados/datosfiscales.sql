fbc93325104150afee459598cba682c3ed6d264c
monchoramon <31085401+monchoramon@users.noreply.github.com>
2020-02-10 18:47:19 -0600

    Script SQL actualizados y Modelo ER Logico y Fisico

---
+++ ScriptsSQLactualizados/datosfiscales.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: 
+-- Date: 2019-11-26 11:59
+*/
+INSERT INTO `datosfiscales` (
+`kcvedatosfiscales`,
+`orfc`,
+`orazonsocial`,
+`rcveestado`,
+`rcvemunicipio`,
+`odireccion`,
+`ocolonia`,
+`ocodigopostal`,
+`oemail`,
+`istatus`,
+`ifecins`,
+`iusrins`,
+`ifecmod`,
+`iusrmod`) VALUES (1,'BABF860315LE4','FERNANDO BARRON BUCIO',15,195,'ISABEL LA CATOLICA #503-D','SAN SEBASTIAN','50150','fernandobarron@solei-mx.com','A','2019-10-10 00:00:00',NULL,NULL,NULL);
---
+++ ScriptsSQLactualizados/producto.sql
@@ -0,0 +1,35 @@
+/*
+-- Query: 
+-- Date: 2019-11-26 12:00
+*/
+INSERT INTO `producto` (`kcveproducto`,
+				`onombre`,
+				`oclave`,
+				`ocostounitario`,
+				`istatus`,
+				`ifecins`,
+				`iusrins`,
+				`ifecmod`,
+				`iusrmod`) VALUES (1,'CREMA 1 LITRO','CRE-1-L',55.10,'A','2019-10-10 00:00:00','1',NULL,NULL);
+
+INSERT INTO `producto` (`kcveproducto`,
+				`onombre`,
+				`oclave`,
+				`ocostounitario`,
+				`istatus`,
+				`ifecins`,
+				`iusrins`,
+				`ifecmod`,
+				`iusrmod`
+) VALUES (2,'TOSTADAS CARIOCAS','TOST-CAR',25.20,'A','2019-10-10 00:00:00','1',NULL,NULL);
+
+INSERT INTO `producto` (`kcveproducto`,
+				`onombre`,
+				`oclave`,
+				`ocostounitario`,
+				`istatus`,
+				`ifecins`,
+				`iusrins`,
+				`ifecmod`,
+				`iusrmod`
+) VALUES (3,'COCA COLA 2 LITROS','COCA-2-L',28.00,'A','2019-10-10 00:00:00','1',NULL,NULL);
